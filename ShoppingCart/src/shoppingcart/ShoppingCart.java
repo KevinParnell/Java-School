14d7dd1c90fa6e3c811754b6c973e634de76db2f
Kevin Parnell <kevinparnell@Kevins-MacBook-Pro.local>
2018-08-22 23:41:36 -0400

    first commit

---
+++ ShoppingCart/src/shoppingcart/ShoppingCart.java
@@ -0,0 +1,13 @@
+package shoppingcart;
+
+public class ShoppingCart {
+
+    public static void main(String[] args) {
+	String custName = "Kevin Parnell";
+	String itemDesc = "shirt";
+	String message;
+
+	message = custName + " wants to purchase a " + itemDesc;
+	System.out.println(message);
+    }
+}
