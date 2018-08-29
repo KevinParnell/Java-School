fe9a71b2d39c135f93108eae9231530ace07c8de
Kevin Parnell <kevinparnell@Kevins-MacBook-Pro.local>
2018-08-29 16:30:43 -0400

    updated code to be neater and be closer to the template released by the teacher

--- Malachi's Pizza Shop/src/pizzashop/pizzashop.java
+++ Malachi's Pizza Shop/src/pizzashop/pizzashop.java
@@ -3,6 +3,7 @@ package pizzashop;
 /*
  Homework 1: Malachi's Pizza Shop
  Program By: Kevin Parnell
+ GitHub: https://github.com/KevinParnell
  File Name: H1_PizzaShop.java
  Function: This program will calculate the number of slices based on a given pizza diameter and slice area.
  */
@@ -14,43 +15,36 @@ public class pizzashop {
 
     public static void main(String[] args) {
 
-        DecimalFormat df;
-        df = new DecimalFormat("#.#");
-        DecimalFormat df1;
-        df1 = new DecimalFormat("#");
+        DecimalFormat oneDec = new DecimalFormat("#.#");
+        DecimalFormat noDec = new DecimalFormat("#");
         // Makes it so that the area won't display more than one decimal spot and no decimal spot
 
         String slice = "slice";
 
-        double diam;
-        double desArea;
+        float pizzaDiam, desArea, numSlice, pizzaRad;
         double area;
-        float numSlice;
-        double pizzaRad;
-        double pi = Math.PI;
 
-        Scanner scanner;
-        scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         // Allows for the user to input information
 
         System.out.println("Welcome to Malachi's Pizza Shop!");
 
         System.out.println("Please enter the diameter of your pizza (inches):");
-        diam = scanner.nextDouble();
+        pizzaDiam = scanner.nextFloat();
 
         System.out.println("Please enter the area of your desired pizza slice (inches sq.):");
-        desArea = scanner.nextDouble();
+        desArea = scanner.nextFloat();
 
-        pizzaRad = diam / 2;
-        area = pi * Math.pow(pizzaRad,2);
+        pizzaRad = pizzaDiam / 2;
+        area = Math.PI * Math.pow(pizzaRad,2);
         numSlice = Math.round(area / desArea);
 
         if (numSlice > 1){
-            slice = slice + "s";
+            slice += "s";
         }
         // Makes it so that if you get more than one slice, it adds the plurality properly
 
-        System.out.println("Your pizza has an area of " + df.format(area) + " inches sq.");
-        System.out.println("You can easily divide your pizza into " + df1.format(numSlice) + " " + slice + " based on your slice area of " + desArea + " inches sq.");
+        System.out.println("Your pizza has an area of " + oneDec.format(area) + " inches sq.");
+        System.out.println("You can easily divide your pizza into " + noDec.format(numSlice) + " " + slice + " based on your slice area of " + desArea + " inches sq.");
     }
 }
