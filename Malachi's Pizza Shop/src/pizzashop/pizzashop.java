605b2ef21d5ab4bc068063d7d8623fb4e1ad210a
Kevin Parnell <kevinparnell@Kevins-MacBook-Pro.local>
2018-08-29 22:42:13 -0400

    made code cleaner and changed where comments are put

--- Malachi's Pizza Shop/src/pizzashop/pizzashop.java
+++ Malachi's Pizza Shop/src/pizzashop/pizzashop.java
@@ -5,7 +5,7 @@ package pizzashop;
  Program By: Kevin Parnell
  GitHub: https://github.com/KevinParnell
  File Name: H1_PizzaShop.java
- Function: This program will calculate the number of slices based on a given pizza diameter and slice area.
+ Function: This program will calculate the number of slices based on a given pizza diameter and slice area
  */
 
 import java.text.DecimalFormat;
@@ -15,17 +15,17 @@ public class pizzashop {
 
     public static void main(String[] args) {
 
+        // Makes it so that the area won't display more than one decimal spot and no decimal spot
         DecimalFormat oneDec = new DecimalFormat("#.#");
         DecimalFormat noDec = new DecimalFormat("#");
-        // Makes it so that the area won't display more than one decimal spot and no decimal spot
 
         String slice = "slice";
 
         float pizzaDiam, desArea, numSlice, pizzaRad;
         double area;
 
-        Scanner scanner = new Scanner(System.in);
         // Allows for the user to input information
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to Malachi's Pizza Shop!");
 
@@ -39,10 +39,10 @@ public class pizzashop {
         area = Math.PI * Math.pow(pizzaRad,2);
         numSlice = Math.round(area / desArea);
 
+        // Makes it so that if you get more than one slice, it adds the plurality properly
         if (numSlice > 1){
             slice += "s";
         }
-        // Makes it so that if you get more than one slice, it adds the plurality properly
 
         System.out.println("Your pizza has an area of " + oneDec.format(area) + " inches sq.");
         System.out.println("You can easily divide your pizza into " + noDec.format(numSlice) + " " + slice + " based on your slice area of " + desArea + " inches sq.");
